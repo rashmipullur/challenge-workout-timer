6d3c063e568973c4416947d254a1c033d8d5fdbb
rashmipullur <mheresince1998@gmail.com>
2024-04-06 00:39:57 +0530

    clousers in function

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -37,6 +37,10 @@ function Calculator({ workouts, allowSound }) {
     },
     [duration, allowSound]
   );
+
+  useEffect(function(){
+    document.title = `your ${number}-exercise workout`
+  },[number])
 
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
